c3e90f0aa1ea7d8e74155add1ceac4ec3d1ee72f
jpbastida <jpbastida@hotmail.com>
2016-12-12 20:17:04 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlProgramNews
+{
+    class Program
+    {
+        static string[] commentsSaved1 = new string[5];
+        static string[] commentsSaved2 = new string[5];
+        static string[] commentsSaved3 = new string[5];
+        static int a = 0, b = 0, c = 0;
+        
+        static void Main(string[] args)
+        {
+            Console.WindowHeight = 42;
+            Console.WindowWidth = 170;
+            while (true)
+            {
+                Strings str = new Strings();
+                showNews();
+                Console.WriteLine();
+                Console.Write("Choose the news to read: ");
+                int optionNews = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                choosedNews(optionNews);
+            }
+        }
+
+        static void showNews()
+        {
+            Strings str = new Strings();
+            string headerNews = "";
+
+            for (int i = 0; i < 50; i++)
+            {
+                headerNews += str.FirstNews[i];
+            }
+            Console.WriteLine("1. " + headerNews);
+            headerNews = "";
+
+            for (int i = 0; i < 50; i++)
+            {
+                headerNews += str.SecondNews[i];
+            }
+            Console.WriteLine("2. " + headerNews);
+            headerNews = "";
+
+            for (int i = 0; i < 50; i++)
+            {
+                headerNews += str.ThirdNews[i];
+            }
+            Console.WriteLine("3. " + headerNews);
+            headerNews = "";
+        }
+
+        static void choosedNews(int option)
+        {
+            Strings str = new Strings();
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("1. Russia praises possible Trump pick Rex ");
+                    Console.WriteLine();
+                    Console.WriteLine(str.FirstNews);
+                    comments(1);
+                    break;
+
+                case 2:
+                    Console.WriteLine("2. Colombia plane crash: passengers were not told to wear seatbelts, survivor says");
+                    Console.WriteLine();
+                    Console.WriteLine(str.SecondNews);
+                    comments(2);
+                    break;
+
+                case 3:
+                    Console.WriteLine("3. Cairo bombing: Sisi names suicide bomber as Coptic Christians protest");
+                    Console.WriteLine();
+                    Console.WriteLine(str.ThirdNews);
+                    comments(3);
+                    break;
+
+                default:
+                    Console.WriteLine("Option unavailable, choose another.");
+                    break;
+            }
+        }
+
+        private static void comments(int fromNews)
+        {
+            switch (fromNews)
+            {
+                case 1:
+                    Console.WriteLine(@"1. Add comment.
+2. See comments.
+");
+                    Console.Write("Choose an option: ");
+                    int option1Com = int.Parse(Console.ReadLine());
+                    switch (option1Com)
+                    {
+                        case 1:
+                            addComent(a);
+                            a++;
+                            break;
+                        case 2:
+                            seeComents(1);
+                            break;
+                    }
+
+                    break;
+
+                case 2:
+                    Console.WriteLine(@"1. Add comment.
+2. See comments.
+");
+                    Console.Write("Choose an option: ");
+                    int option2Com = int.Parse(Console.ReadLine());
+                    switch (option2Com)
+                    {
+                        case 1:
+                            addComent2(b);
+                            b++;
+                            break;
+                        case 2:
+                            seeComents(2);
+                            break;
+                    }
+                    break;
+
+                case 3:
+                    Console.WriteLine(@"1. Add comment.
+2. See comments.
+");
+                    Console.Write("Choose an option: ");
+                    int option3Com = int.Parse(Console.ReadLine());
+                    switch (option3Com)
+                    {
+                        case 1:
+                            addComent3(c);
+                            c++;
+                            break;
+                        case 2:
+                            seeComents(3);
+                            break;
+                    }
+                    break;
+            }
+        }
+
+        private static void seeComents(int news)
+        {
+            int x = 1;
+            if (news == 1)
+            {
+                foreach (var comment in commentsSaved1)
+                {
+                    Console.Write(x);
+                    Console.WriteLine(comment);
+                    Console.WriteLine();
+                    x++;
+                }
+            }
+
+            if (news == 2)
+            {
+                foreach (var comment in commentsSaved2)
+                {
+                    Console.Write(x);
+                    Console.WriteLine(comment);
+                    Console.WriteLine();
+                    x++;
+                }
+            }
+            if (news == 3)
+            {
+                foreach (var comment in commentsSaved3)
+                {
+                    Console.Write(x);
+                    Console.WriteLine(comment);
+                    Console.WriteLine();
+                    x++;
+                }
+            }
+
+        }
+
+        private static void addComent(int index)
+        {
+            
+            string comm = Console.ReadLine();
+            Console.WriteLine("Comment: " + comm);
+            commentsSaved1[index] = comm;
+            Console.WriteLine();
+        }
+
+        private static void addComent2(int index)
+        {
+
+            string comm = Console.ReadLine();
+            Console.WriteLine("Comment: " + comm);
+            commentsSaved2[index] = comm;
+            Console.WriteLine();
+        }
+
+        private static void addComent3(int index)
+        {
+
+            string comm = Console.ReadLine();
+            Console.WriteLine("Comment: " + comm);
+            commentsSaved3[index] = comm;
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Strings.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlProgramNews
+{
+    public class Strings
+    {
+        public string Menu = (@"
+1. USA Elections!
+2. Fidel Castro died!
+3. NASA sent new astronauts!");
+
+
+        public string FirstNews = (@"The Kremlin has praised the professionalism of Rex Tillerson, thought to beDonald Trump’s leading contender for 
+secretary of state, the ExxonMobil CEO who has forged close ties to Russia.
+“On account of his work as the head of one of the largest oil companies, he had contacts with our representatives more than once,” P
+resident Vladimir Putin’s spokesman Dmitry Peskov told journalists on Monday. “He fulfills his responsibilities in a highly professional manner.”
+Peskov also said “there’s a big difference being secretary of state and directing even a very big company” and that the Kremlin hoped for a 
+“readiness to demonstrate a constructive attitude and display professionalism” from Washington.
+Putin had hosted Tillerson on several occasions, Peskov added. The Russian president honoured the oil executive with an order of friendship 
+award in 2013.
+Although Trump has not yet nominated Tillerson, several news outlets have reported he is likely to do so in the next few days.");
+
+        public string SecondNews = (@"A survivor of a plane crash in Colombia that killed 71 people says the pilot never told passengers to 
+fasten their seatbelt before the aircraft smashed into a hillside.
+Brazilian radio journalist Rafael Henzel spoke with Fantastico program Sunday night from Colombia, where he is recovering.
+“At no point did someone from the cabin or the crew tell us, ‘Put on your seatbelts’,” said Henzel. “We just kept flying without any 
+idea of what was about to happen.”
+The plane took off from Santa Cruz, Bolivia and crashed near the airport in Medellín, Colombia on 28 November. Only six of the 77 passengers 
+on board survived. The dead included 19 members of the Chapecoense soccer club from southern Brazil and 20 of the journalists covering the team. 
+The survivors were two Bolivian crew members and four Brazilians – Henzel and three soccer players.
+Investigations have been launched in Brazil, Bolivia and Colombia. Colombian authorities have said they believe a lack of fuel caused the crash.
+Henzel said that toward the end of the flight, passengers around him began asking how long it would be before they landed.
+“Ten minutes” was the response from the crew, he said. “Then suddenly, the lights went off and the engines went off.”
+At that point, everyone rushed back to their seats and fastened their seatbelts, said Henzel. Then the plane crashed into a hillside.
+Henzel said when he awoke, there were emergency workers around him and he was in immense pain from what turned out to be seven broken ribs. 
+He called out to the colleagues who had been sitting on each side of him, but they had died.
+He said he believed he survived thanks to a miracle and the good fortune to wake up just as emergency workers passed by.
+Henzel said he was sickened to learn the cause of the accident was likely a lack of fuel.
+");
+
+        public string ThirdNews = (@"Mourners have paid their respects to the victims of Sunday’s attack on the seat of Coptic Christianity in Egypt, 
+as the Coptic pope sought to heal sectarian frictions amid rising anger from a minority that claims it is insufficiently protected. 
+The attack on St Peter and St Paul church, adjoining Cairo’s St Mark’s Cathedral, took place during Sunday morning prayers and killed 24 people, 
+most of them women and children, according to an updated toll issued by the health ministry on Monday. At least 45 others were injured. 
+There has been no claim of responsibility.
+President Abdel-Fatah al-Sisi named a 22-year-old, Mahmoud Shafik Mohamed Mostafa, as the alleged perpetrator of the suicide attack. 
+He said other people, including one woman, had been arrested over their alleged involvement.
+As mourners crowded into the Virgin Mary and St Athanasius church in Cairo on Monday, the Coptic pope, Tawadros II, attempted to focus on 
+the unity of Egypt’s grief, denouncing the blast as “not just a disaster for the church but a disaster for the whole nation”.
+");
+
+
+
+    }
+}
